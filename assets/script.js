d131e61d229101852433c5a9a0bdb9c3265bfc44
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2022-08-24 17:44:36 +1000

    Init Commit

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+const scriptRemote = require('electron').remote;
+
+$(document).ready(function(){
+
+    $("#intro").fadeIn(1000).delay(1000)
+    $("#introBtn").fadeIn(1000).delay(1000)
+
+    document.getElementById("closeBtn").addEventListener("click", function (e) {
+        var remoteWindow = scriptRemote.getCurrentWindow();
+        remoteWindow.close();
+    }); 
+});
+
+function startInstall() {
+    $("#introBtn").fadeOut(500);
+    $("#intro").fadeOut(function(){
+        $("#load").fadeIn(1000);
+        $(".loader").fadeIn(1000);
+    });
+}
+
+function finished(){
+    
+    $("#load").fadeOut(function(){
+        $(".loader").fadeOut(1000);
+        $("#done").fadeIn(1000).delay(2000).fadeOut(function(){
+
+            $("#close").fadeIn(1000);
+
+        });
+    });
+
+}
